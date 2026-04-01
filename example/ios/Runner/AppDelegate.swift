208def9245f6f9b6498a9083289c1a100cd3feb4
RealSid45 <sidhuxplore4@gmail.com>
2026-04-01 16:05:21 +0530

    Added iOS plugin skeleton and AVFoundation camera pipeline

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,13 +1,62 @@
-import Flutter
 import UIKit
+import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    // Camera manager instance
+    var cameraManager = CameraManager()
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        let controller = window?.rootViewController as! FlutterViewController
+        
+        // Create MethodChannel
+        let channel = FlutterMethodChannel(
+            name: "biometric_sdk",
+            binaryMessenger: controller.binaryMessenger
+        )
+        
+        // Handle Flutter calls
+        channel.setMethodCallHandler { (call, result) in
+            
+            switch call.method {
+                
+            case "processFrame":
+                print("iOS: processFrame called")
+                
+                // Start camera session
+                self.cameraManager.startSession()
+                
+                // Send temporary response to Flutter
+                result([
+                    "fingerDetected": false,
+                    "guidance": "Camera started",
+                    "qualityScore": 0,
+                    "fingerCount": 0,
+                    "readyToCapture": false
+                ])
+                
+            case "startCapture":
+                print("iOS: startCapture called")
+                
+                result([
+                    "transactionId": "ios-test",
+                    "overallStatus": "success",
+                    "results": []
+                ])
+                
+            default:
+                result(FlutterMethodNotImplemented)
+            }
+        }
+        
+        // Register Flutter plugins
+        GeneratedPluginRegistrant.register(with: self)
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
